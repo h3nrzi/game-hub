36f7c309fb944f89d8cd5aa7a4a24a05bad7a157
Hossein <rezaeig22@gmail.com>
2024-05-21 20:37:04 +0330

    Implement searching games

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
 	genre: Genre | null;
 	platform: Platform | null;
 	sortOrder: string;
+	searchText: string;
 }
 
 const App = () => {
@@ -29,7 +30,9 @@ const App = () => {
 			}}
 		>
 			<GridItem area="nav">
-				<Navbar />
+				<Navbar
+					onSearch={(s) => setGameQuery({ ...gameQuery, searchText: s })}
+				/>
 			</GridItem>
 			<Show above="lg">
 				<GridItem
