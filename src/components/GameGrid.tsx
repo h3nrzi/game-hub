cf1506206ef876dd3045631ab50a010d37142b38
Hossein <rezaeig22@gmail.com>
2024-05-24 11:31:28 +0330

    Implementing Infinite Queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Fragment } from "react";
+import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -6,29 +7,49 @@ import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
 
 const GameGrid = ({ gameQuery }: { gameQuery: GameQuery }) => {
-	const { data, error, isLoading } = useGames(gameQuery);
+	const {
+		data,
+		error,
+		isLoading,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 	if (error) return <Text>{error.message}</Text>;
 
 	return (
-		<SimpleGrid
-			columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-			spacing="15px"
-			padding="10px"
-		>
-			{isLoading &&
-				skeletons.map((skeleton) => (
-					<GameCardContainer key={skeleton}>
-						<GameCardSkeleton />
-					</GameCardContainer>
+		<Box p="10px">
+			<SimpleGrid
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				spacing="15px"
+			>
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GameCardContainer key={skeleton}>
+							<GameCardSkeleton />
+						</GameCardContainer>
+					))}
+				{data?.pages.map((page, index) => (
+					<Fragment key={index}>
+						{page.results.map((game) => (
+							<GameCardContainer key={game.id}>
+								<GameCard game={game} />
+							</GameCardContainer>
+						))}
+					</Fragment>
 				))}
-			{data?.results.map((game) => (
-				<GameCardContainer key={game.id}>
-					<GameCard game={game} />
-				</GameCardContainer>
-			))}
-		</SimpleGrid>
+			</SimpleGrid>
+			{hasNextPage && (
+				<Button
+					onClick={() => fetchNextPage()}
+					my="30px"
+				>
+					{isFetchingNextPage ? "Loading..." : "Load More"}
+				</Button>
+			)}
+		</Box>
 	);
 };
 
